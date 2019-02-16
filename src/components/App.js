0928c0eeacd6163edd9ae3ae3fb7565bfd75c1f3
mattdknapp <matthewdknapp@gmail.com>
2019-02-16 15:37:55 -0500

    Drop it in a card.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import Alert from 'react-s-alert'
 import 'react-s-alert/dist/s-alert-default.css'
 import 'react-s-alert/dist/s-alert-css-effects/slide.css'
+import './App.css'
 
 import Tree from './tree/Tree'
 import store from '../redux/store'
@@ -13,9 +14,21 @@ class App extends Component {
     return (
       <div className="App">
         <Provider store={store}>
-          <h3>Factory App</h3>
-          <div className="d-flex justify-content-center">
-            <Tree/>
+          <div className="container">
+            <div className="row">
+              <div className="col-1"/>
+              <div className="col-10">
+                <div className="card">
+                  <div className="card-header">
+                    <h3>Factory App</h3>
+                  </div>
+                  <div className="card-body">
+                    <Tree/>
+                  </div>
+                </div>
+              </div>
+              <div className="col-1"/>
+            </div>
           </div>
           <Alert stack={alertConfig}/>
         </Provider>
