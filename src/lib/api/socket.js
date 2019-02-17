5124169fbb0e582593b8c4cdec83d3dbef25103e
mattdknapp <matthewdknapp@gmail.com>
2019-02-17 09:51:30 -0500

    And make sure we never poll.

--- src/lib/api/socket.js
+++ src/lib/api/socket.js
@@ -1,6 +1,11 @@
 import io from 'socket.io-client'
 import config from '../../config'
 
-const socket = io(config.socketUrl)
+const opts = {
+  transports: ['websocket'],
+  upgrade: false
+}
+
+const socket = io(config.socketUrl, opts)
 
 export default socket
